fa5a823891b74c3e0fbf049982fe48f9c4d4a2c6
Joshua-likesjs <galego.jjdocarmo@gmail.com>
2025-11-19 21:32:59 -0400

    mobile pra vercel com redirecionamento

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,21 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
-  const ua = req.headers.get("user-agent")?.toLowerCase() || "";
+  const ua = req.headers.get("user-agent")?.toLowerCase()
 
   const isMobile =
-    ua.includes("iphone") ||
-    ua.includes("android") ||
-    ua.includes("ipad") ||
-    ua.includes("mobile");
+    ua?.includes("iphone") ||
+    ua?.includes("android") ||
+    ua?.includes("ipad")
 
-  // Se for mobile e estiver tentando acessar a página web:
-  if (isMobile && req.nextUrl.pathname === "/") {
-    const url = req.nextUrl.clone();
-    url.pathname = "/mobile"; // rota mobile
-    return NextResponse.redirect(url);
+  if (isMobile) {
+    return NextResponse.redirect("https://seu-mobile.vercel.app")
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/"],
-};
+  matcher: '/'
+}
